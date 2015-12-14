60d3537b49230c16627457043b51e3176e8fb08e
Glyn Normington <gnormington@pivotal.io>
2015-12-14 12:09:42 +0000

    Make test test something

--- src/test/java/org/cfguardian/learningstreams/LearningStreamsTest.java
+++ src/test/java/org/cfguardian/learningstreams/LearningStreamsTest.java
@@ -6,16 +6,17 @@ import java.util.List;
 import org.junit.Test;
 import reactor.rx.Streams;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by spowell on 11/12/2015.
  */
 public final class LearningStreamsTest {
 	
 	@Test
-	public final void basic() {
-		System.out.println(""+Streams.from(getSimpleList()).next().get());
-	} 
-
+	public final void streamFromIterable() {
+		assertEquals("singleton element 1", Streams.from(getSimpleList()).next().get());
+	}
 	
 	List<String> getSimpleList() {
 		return Collections.singletonList("singleton element 1");
