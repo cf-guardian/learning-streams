a65c85bea18b7ed13accc9dbfc85c428c6764417
Glyn Normington <gnormington@pivotal.io>
2015-12-14 16:45:05 +0000

    Add stream construction techniques
    ... and depend on snapshot

--- src/test/java/org/cfguardian/learningstreams/LearningStreamsTest.java
+++ src/test/java/org/cfguardian/learningstreams/LearningStreamsTest.java
@@ -1,10 +1,12 @@
 package org.cfguardian.learningstreams;
 
+import org.junit.Test;
+import org.reactivestreams.Publisher;
+import reactor.rx.Streams;
+import reactor.rx.stream.DecoratingStream;
+
 import java.util.Collections;
 import java.util.List;
-
-import org.junit.Test;
-import reactor.rx.Streams;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,13 +14,27 @@ import static org.junit.Assert.assertEquals;
  * Created by spowell on 11/12/2015.
  */
 public final class LearningStreamsTest {
-	
-	@Test
-	public final void streamFromIterable() {
-		assertEquals("singleton element 1", Streams.from(getSimpleList()).next().get());
-	}
-	
-	List<String> getSimpleList() {
-		return Collections.singletonList("singleton element 1");
-	}
+
+    @Test
+    public final void streamFromIterable() {
+        assertEquals("singleton element 1", Streams.from(getSimpleList()).next().get());
+    }
+
+    @Test
+    public final void publisherToStream() {
+        Publisher<String> p = Streams.from(getSimpleList());
+
+        assertEquals("singleton element 1", new DecoratingStream<>(p).next().get());
+    }
+
+    @Test
+    public final void wrapPublisher() {
+        Publisher<String> p = Streams.from(getSimpleList());
+
+        assertEquals("singleton element 1", Streams.wrap(p).next().get());
+    }
+
+    List<String> getSimpleList() {
+        return Collections.singletonList("singleton element 1");
+    }
 }
