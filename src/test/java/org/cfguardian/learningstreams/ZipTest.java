859e79f43f23491e80db7f79d761e9b45546d6d9
Glyn Normington <gnormington@pivotal.io>
2015-12-23 16:53:31 +0000

    More zip tests

--- src/test/java/org/cfguardian/learningstreams/ZipTest.java
+++ src/test/java/org/cfguardian/learningstreams/ZipTest.java
@@ -1,24 +1,50 @@
 package org.cfguardian.learningstreams;
 
-import static org.cfguardian.learningstreams.LearningStreams.optional;
-import static org.junit.Assert.*;
-
-import java.util.Optional;
-
 import org.junit.Test;
 import reactor.rx.Stream;
 import reactor.rx.Streams;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.cfguardian.learningstreams.LearningStreams.optional;
+import static org.junit.Assert.*;
+
 public final class ZipTest {
 
-	@Test
-	public void compareStreams() {
+    @Test
+    public void compareStreams() {
 
-		Stream<String> as = Streams.just("x", "y");
-		Stream<String> bs = Streams.just("x");
+        Stream<String> as = Streams.just("x", "y");
+        Stream<String> bs = Streams.just("x");
 
-		assertFalse(Streams.zip(optional(as), optional(bs), (a, b) -> a.equals(b))
-				.reduce(true, (accum, match) -> accum && match)
-				.next().get());
-	}
+        assertFalse(Streams.zip(optional(as), optional(bs), (a, b) -> a.equals(b))
+                .reduce(true, (accum, match) -> accum && match)
+                .next().get());
+    }
+
+    @Test
+    public void zipOfNull() {
+
+        Stream<String> as = Streams.just("x");
+        Stream<String> bs = Streams.<String>just(null);
+        final AtomicBoolean ran = new AtomicBoolean(false);
+
+        // This should fail rather than deliver a null value.
+        assertNull(Streams.zip(as, bs, (a, b) -> {
+            ran.set(true);
+            return "y";
+        }).next().get());
+
+        assertFalse(ran.get());
+    }
+
+    @Test
+    public void simpleZip() {
+
+        Stream<String> as = Streams.just("x");
+        Stream<String> bs = Streams.just("y");
+
+        assertTrue(Streams.zip(as, bs, (a, b) -> "x".equals(a) && "y".equals(b))
+                .next().get());
+    }
 }
